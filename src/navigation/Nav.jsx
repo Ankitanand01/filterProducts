3ffd9a1b9d485c162e3c5e68bffcb2c8f38f3ac1
Your Name <ankitanandgupta260@gmail.com>
2023-07-06 13:47:58 +0530

    style update

--- src/navigation/Nav.jsx
+++ src/navigation/Nav.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { AiOutlineHeart, AiOutlineUserAdd, AiOutlineShoppingCart } from "react-icons/ai";
 import './Nav.scss'
 const Nav = () => {
     return (
         <div>
-            <h1>Navigation</h1>
+            <nav>
+                <div className="nav-container">
+                    <input type="text" className='search-input' name="" id="" placeholder='Enter your search shoes' />
+                </div>
+
+                <div className="profile-container">
+                    <a href="https://flipkart.com">
+                        <AiOutlineHeart className='nav-icons' />
+                    </a>
+                    <a href="https://flipkart.com">
+                        <AiOutlineUserAdd className='nav-icons' />
+                    </a>
+                    <a href="https://flipkart.com">
+                        <AiOutlineShoppingCart className='nav-icons' />
+                    </a>
+                </div>
+            </nav>
         </div>
     )
 }
